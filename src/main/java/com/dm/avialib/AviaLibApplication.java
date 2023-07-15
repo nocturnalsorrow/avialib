50aeea4474ee5c181d12f8162fde613c0538f0cd
Danial Rehman <danial.rehman36@gmail.com>
2023-07-15 21:22:11 +0300

    Added header and footer

--- src/main/java/com/dm/avialib/AviaLibApplication.java
+++ src/main/java/com/dm/avialib/AviaLibApplication.java
@@ -3,11 +3,12 @@ package com.dm.avialib;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Date;
+
 @SpringBootApplication
 public class AviaLibApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AviaLibApplication.class, args);
     }
-
 }
---
+++ src/main/resources/templates/html/js/headerfooterloader.js
@@ -0,0 +1,4 @@
+$(function(){
+    $("#header-placeholder").load("header.html"); // Загрузка содержимого заголовка
+    $("#footer-placeholder").load("footer.html"); // Загрузка содержимого нижнего колонтитула
+});
