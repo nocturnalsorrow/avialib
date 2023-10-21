76314f4cf01721547382528580cbe2e35daf2751
Danial Rehman <danial.rehman36@gmail.com>
2023-10-21 19:09:34 +0300

    Updated GlobalExceptionHandler.java

--- src/main/java/com/dm/avialib/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/dm/avialib/exceptions/GlobalExceptionHandler.java
@@ -3,16 +3,18 @@ package com.dm.avialib.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ArticleNotFoundException.class)
     public ApiException handleArticleNotFoundException(ArticleNotFoundException ex) {
         return new ApiException(HttpStatus.NOT_FOUND,
-                ex.getMessage(),
+                "Article not found",
                 ZonedDateTime.now(ZoneId.of("Z")));
     }
 }
